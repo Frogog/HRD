8e4e157aaba0f164f38d0ed9b38078fff628f6b1
123home <123home@DESKTOP-RR794IQ>
2024-11-30 07:22:37 +0500

    Попытка пофикусить форму

--- ShowAllEmployeeForm.Designer.cs
+++ ShowAllEmployeeForm.Designer.cs
@@ -794,7 +794,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(845, 711);
+            this.ClientSize = new System.Drawing.Size(845, 731);
             this.Controls.Add(this.confirmB);
             this.Controls.Add(this.mainLable);
             this.Controls.Add(this.canselB);
